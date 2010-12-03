6e17d6ff579dab8c3ad59a5024919437a9b9eff4
Kate Thibault <kate@weecology.org>
2010-12-03 19:38:06 +0000

    MySQL query to extract BBS data for METE analyses. Yields a table with the fields, Year - SiteID - Sp - abund, where the species is represented by the TOO (TAXON_ORDER_OUT) value from the TAXONOMY_BIRDS.TAXONOMY table. Current version extracts only data for 2009.

---
+++ data/BBS_data_query.py
@@ -0,0 +1,61 @@
+"""MySQL Querying of BBS to yield data for METE analyses"""
+
+import MySQLdb as dbapi
+import getpass
+
+# enter MySQL password
+p=getpass.getpass()
+
+connection = dbapi.connect(host='129.123.92.244', port=1977, user='kate', 
+                       passwd=p)
+cursor = connection.cursor()
+
+cursor.execute("""DROP DATABASE IF EXISTS queries;""")
+cursor.execute("""CREATE DATABASE queries;""")
+
+# Step 1. TAXONOMY_BIRDS TAXONOMY.AOU_IN linked to BBS counts.Aou
+    # Left Join = ALL records from BBS counts are included 
+    # and only values from TAXONOMY that match
+# Step 2. Group By AOU IN and by TOO WHERE DIURNAL LANDBIRD = 1 - 
+    # to yield AOU_TOO
+
+cursor.execute("""
+                CREATE TABLE queries.aous (aou INT(11)) 
+                SELECT counts.Aou FROM BBS.counts 
+                GROUP BY counts.Aou;
+                """) 
+
+cursor.execute("""
+                CREATE TABLE queries.aou_too_1 (aou INT(11)) 
+                SELECT Aou AS AOU, TAXON_ORDER_OUT AS TOO FROM queries.aous 
+                LEFT JOIN TAXONOMY_BIRDS.TAXONOMY 
+                ON aous.Aou = TAXONOMY.AOU_IN 
+                WHERE TAXONOMY.DIURNALLANDBIRD = 1;
+                """) 
+
+cursor.execute("""
+                CREATE TABLE queries.aou_too (aou INT(11)) 
+                SELECT AOU, TOO FROM queries.aou_too_1 
+                GROUP BY AOU, TOO;
+                """) 
+
+# 3. To create table with Year - SiteID - Sp - abund:
+    # Link together AOU_TOO and BBS Counts by AOU - 
+    # Group By SiteID = state*1000 + route - TOO - Year 
+    # Sum SpeciesTotal
+    
+cursor.execute("""
+                CREATE TABLE queries.bbs_too
+                SELECT (counts.statenum * 1000) + counts.Route AS SiteID,
+                counts.Year, aou_too.TOO, 
+                SUM(counts.SpeciesTotal) AS AB 
+                FROM BBS.counts INNER JOIN queries.aou_too ON counts.Aou = aou_too.AOU
+                GROUP BY (counts.statenum * 1000) + counts.Route, counts.Year, aou_too.TOO
+                HAVING (((counts.Year) = 2009));
+                """)
+
+# I tried to execute the last query as a SELECT....INTO OUTFILE query,
+# but I ran into permissions problems (error code 13). Temporary fix is to use
+# MySQL Workbench to export the results of the query to a csv file. 
+                
+connection.commit()
