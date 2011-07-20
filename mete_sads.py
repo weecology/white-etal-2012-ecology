781fab2a853d358970f6062edd94c4826e270090
Ethan White <ethan@weecology.org>
2011-07-20 21:13:38 +0000

    Allow simulations to be easily executed from the command line.

--- mete_sads.py
+++ mete_sads.py
@@ -639,7 +639,10 @@ if __name__ == '__main__':
                        workdir + 'gentry_dist_test.csv',
                        workdir + 'mcdb_dist_test.csv',
                        workdir + 'naba_dist_test.csv')
-        
-        print "Soon this will conduct simulations, but now I have get River up"
+        for current_file in input_filenames:
+            match = re.search('/([a-z]*)_', current_file)
+            data_id = match.group(1)
+            create_null_dataset(current_file, workdir + data_id + '_sim_r2.csv',
+                                1)
     else:
         print "The second argument should be either empir or sim. See the docs"
