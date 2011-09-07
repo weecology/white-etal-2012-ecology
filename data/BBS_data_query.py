deb0696fcad5b4b193f7e9567bc9379cd559d005
Ben Morris <ben@bendmorris.com>
2011-09-07 23:20:56 +0000

    Updating BBS data query to use get_data function.

--- data/BBS_data_query.py
+++ data/BBS_data_query.py
@@ -1,80 +1,52 @@
 """MySQL Querying of BBS to yield data for METE analyses"""
 
-import MySQLdb as dbapi
-import getpass
-import shutil
+from get_data import get_data
 
-# enter MySQL password
-p=getpass.getpass()
-
-connection = dbapi.connect(host='129.123.92.244', port=1977, user='kate', 
-                       passwd=p)
-cursor = connection.cursor()
-
-cursor.execute("""DROP DATABASE IF EXISTS queries;""")
-cursor.execute("""CREATE DATABASE queries;""")
-
+get_data([
 # Step 1. TAXONOMY_BIRDS TAXONOMY.AOU_IN linked to BBS counts.Aou
     # Left Join = ALL records from BBS counts are included 
     # and only values from TAXONOMY that match
 # Step 2. Group By AOU IN and by TOO WHERE DIURNAL LANDBIRD = 1 - 
     # to yield AOU_TOO
-
-cursor.execute("""
-                CREATE TABLE queries.aous (aou INT(11)) 
-                SELECT counts.Aou FROM BBS.counts 
-                GROUP BY counts.Aou;
-                """) 
-
-cursor.execute("""
-                CREATE TABLE queries.aou_too_1 (aou INT(11)) 
-                SELECT Aou AS AOU, TAXON_ORDER_OUT AS TOO FROM queries.aous 
-                LEFT JOIN TAXONOMY_BIRDS.TAXONOMY 
-                ON aous.Aou = TAXONOMY.AOU_IN 
-                WHERE TAXONOMY.DIURNALLANDBIRD = 1;
-                """) 
-
-cursor.execute("""
-                CREATE TABLE queries.aou_too (aou INT(11)) 
-                SELECT AOU, TOO FROM queries.aou_too_1 
-                GROUP BY AOU, TOO;
-                """) 
-
+          """CREATE TABLE queries.aous (aou INT(11)) 
+             SELECT counts.Aou FROM BBS.counts 
+             GROUP BY counts.Aou;
+             """,
+          """CREATE TABLE queries.aou_too_1 (aou INT(11)) 
+             SELECT Aou AS AOU, TAXON_ORDER_OUT AS TOO FROM queries.aous 
+             LEFT JOIN TAXONOMY_BIRDS.TAXONOMY 
+             ON aous.Aou = TAXONOMY.AOU_IN 
+             WHERE TAXONOMY.DIURNALLANDBIRD = 1;
+             """,
+          """CREATE TABLE queries.aou_too (aou INT(11)) 
+             SELECT AOU, TOO FROM queries.aou_too_1 
+             GROUP BY AOU, TOO;
+             """,
 # 3. To create table with SiteID - Year - RunType = 1:
     # from weather table
-    
-cursor.execute("""
-                CREATE TABLE queries.weather_subquery
-                SELECT (weather.statenum*1000+ weather.Route) AS SiteID, weather.Year, weather.RunType
-                FROM BBS.weather
-                WHERE weather.RunType = 1 AND weather.RPID = 101;
-                """)
-                 
+          """CREATE TABLE queries.weather_subquery
+             SELECT (weather.statenum*1000+ weather.Route) AS SiteID, weather.Year, weather.RunType
+             FROM BBS.weather
+             WHERE weather.RunType = 1 AND weather.RPID = 101;
+             """,
 # 4. To create table with SiteID - Year - Sp - abund:
     # Link together AOU_TOO and BBS Counts by AOU - 
     # Group By SiteID = state*1000 + route - TOO - Year 
     # Sum SpeciesTotal
-    
-cursor.execute("""
-                CREATE TABLE queries.counts_too
-                SELECT (counts.statenum * 1000) + counts.Route AS SiteID,
-                counts.Year, aou_too.TOO, 
-                SUM(counts.SpeciesTotal) AS AB 
-                FROM BBS.counts INNER JOIN queries.aou_too ON counts.Aou = aou_too.AOU
-                GROUP BY SiteID, counts.Year, aou_too.TOO, counts.RPID
-                HAVING (((counts.Year = 2009) AND (counts.RPID = 101)));
-                """)
-                
-cursor.execute("""
-                SELECT counts_too.SiteID, counts_too.Year, counts_too.TOO, counts_too.AB
-                FROM queries.counts_too INNER JOIN queries.weather_subquery
-                ON counts_too.SiteID = weather_subquery.SiteID 
-                AND counts_too.Year = weather_subquery.Year
-                INTO OUTFILE '/tmp/bbs_too_2009.csv'
-                FIELDS TERMINATED BY ',' 
-                LINES TERMINATED BY '\n';
-                """)
-                
-connection.commit()
-
-shutil.copy('/tmp/bbs_too_2009.csv', '/home/kate/data/bbs_too_2009.csv')
+          """CREATE TABLE queries.counts_too
+             SELECT (counts.statenum * 1000) + counts.Route AS SiteID,
+             counts.Year, aou_too.TOO, 
+             SUM(counts.SpeciesTotal) AS AB 
+             FROM BBS.counts INNER JOIN queries.aou_too ON counts.Aou = aou_too.AOU
+             GROUP BY SiteID, counts.Year, aou_too.TOO, counts.RPID
+             HAVING (((counts.Year = 2009) AND (counts.RPID = 101)));
+             """,
+          """SELECT counts_too.SiteID, counts_too.Year, counts_too.TOO, counts_too.AB
+             FROM queries.counts_too INNER JOIN queries.weather_subquery
+             ON counts_too.SiteID = weather_subquery.SiteID 
+             AND counts_too.Year = weather_subquery.Year
+             INTO OUTFILE '/tmp/bbs_too_2009.csv'
+             FIELDS TERMINATED BY ',' 
+             LINES TERMINATED BY '\n';
+             """,
+])
